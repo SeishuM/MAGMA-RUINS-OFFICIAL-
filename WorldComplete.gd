27237ff3e7c5c2ade9e30687e7751cd47b27b9ca
Haval <havalm3@gmail.com>
2019-06-26 11:36:24 -0400

    Deleted extra .gd files

--- WorldComplete.gd
+++
@@ -1,8 +0,0 @@
-# WorldComplete.gd
-extends Area2D
-
-func _physics_process(delta):
-	var bodies = get_overlapping_bodies()
-	for body in bodies:
-		if body.name == "Player":
-			get_tree().change_scene("World.tscn")
